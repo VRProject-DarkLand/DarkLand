f1c7de7179530ea3904ae3a1b61a60024e734246
bfrancesco <brunofrancescobarbara@gmail.com>
2024-04-27 12:17:38 +0200

    added door opener

--- DarkLand/Assets/Scripts/OpenDoor.cs
+++ DarkLand/Assets/Scripts/OpenDoor.cs
@@ -5,11 +5,16 @@ using UnityEngine;
 
 public class OpenDoor : MonoBehaviour
 {
+    private Quaternion open;
+    private Quaternion close;
     private bool opened ;
+    [SerializeField] private GameObject door;
     // Start is called before the first frame update
     void Start()
     {
         opened = false;
+        close  = door.transform.rotation;
+        open = close*Quaternion.Euler(0, door.transform.rotation.y-90f, 0);
     }
 
     // Update is called once per frame
@@ -18,16 +23,22 @@ public class OpenDoor : MonoBehaviour
         
     }
 
-    public void Open(){
-        if(gameObject != null && !opened){
-            Debug.Log("CIAO");
-            gameObject.transform.rotation*=Quaternion.Euler(0, 90f, 0);
-            opened = true;
+    public void ChangeState(){
+        if(door != null){
+            door.transform.rotation = opened ? close : open; 
+            opened = !opened;        
         }
     }
 
     void OnTriggerEnter(Collider collider){
-        Open();
-        Debug.Log("mannaja di");
+        if(Input.GetKeyDown(KeyCode.E)){
+            ChangeState();
+        } 
     }
+    void OnTriggerStay(Collider collider){
+        if(Input.GetKeyDown(KeyCode.E)){
+            ChangeState();
+        }  
+    }
+
 }
---
+++ DarkLand/Assets/Scripts/OpenDoubleDoor.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenDoubleDoor : MonoBehaviour
+{
+    private Tuple<Quaternion,Quaternion> open;
+    private Tuple<Quaternion,Quaternion> close;
+    private bool opened ;
+    [SerializeField] private GameObject left;
+    [SerializeField] private GameObject right;
+    // Start is called before the first frame updatevoid Start()
+    void Start(){
+        opened = false;
+        close  = Tuple.Create(left.transform.rotation, right.transform.rotation);
+        open = Tuple.Create(close.Item1*Quaternion.Euler(0, left.transform.rotation.y-90f, 0), close.Item2*Quaternion.Euler(0, right.transform.rotation.y-90f, 0));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ChangeState(){
+        if(left != null && right != null){
+            if(opened){
+                 left.transform.rotation = close.Item1 ;
+                 right.transform.rotation = close.Item2 ;
+            }else{
+                left.transform.rotation = open.Item1 ;
+                right.transform.rotation = open.Item2 ;
+            }
+            opened = !opened;        
+        }
+    }
+
+    void OnTriggerEnter(Collider collider){
+        if(Input.GetKeyDown(KeyCode.E)){
+            ChangeState();
+        } 
+    }
+    void OnTriggerStay(Collider collider){
+        if(Input.GetKeyDown(KeyCode.E)){
+            ChangeState();
+        }  
+    }
+}
