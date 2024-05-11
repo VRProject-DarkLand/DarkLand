4dc72e5ed9c0830c39398338c3c07bade417afbf
ndria00 <ctrndr00e01i872o@studenti.unical.it>
2024-05-11 18:17:52 +0200

    fixed falling behaviour

--- DarkLand/Assets/Scripts/FPSInput.cs
+++ DarkLand/Assets/Scripts/FPSInput.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UIElements;
 [RequireComponent(typeof (CharacterController))]
 [AddComponentMenu("Control Script/FPS Input")]
 public class FPSInput : MonoBehaviour{    
@@ -10,23 +11,25 @@ public class FPSInput : MonoBehaviour{
     private const float CROUCH_SPEED = 2.0f;
     private const float RUN_SPEED = 5.5f;
     public float speed = 4.0f;
-    private float jumpSpeed = 10f;
-    private float minFall = -1.5f;
-    private float _ySpeed ;
-    private float gravity = -9.81f;
+    private float _minFall = -1.5f;
+    private float jumpHeight = 1f;
+    private float gravity = -9.8f;
     private float airResistance = -0.2f;
-    private float terminalVelocity = -15f;
+    private float _terminalVelocity = -15f;
+    private float deltaY = 0;
     private float deltaX = 0;
     private float deltaZ = 0;
     private float dirX; 
     private float dirZ; 
-    private bool onGround = false;
+    public bool onGround = false;
     private bool crouch = false;
     private CharacterController _charController;
-    private ControllerColliderHit _contact; 
     private Camera _camera;
     private List<Actions> actions;
-    private Quaternion jumpRotation;
+    private ControllerColliderHit _contact; 
+    private Quaternion _jumpRotation;
+    public float temp;
+    private bool _jumping;
     // Start is called before the first frame update
     void Start()
     {
@@ -34,6 +37,7 @@ public class FPSInput : MonoBehaviour{
         _camera = GetComponentInChildren<Camera>();
         actions = new List<Actions>(){Actions.Idle};
         gameObject.tag = Settings.PLAYER_TAG;
+        _jumping = false;
     }
 
 
@@ -55,34 +59,29 @@ public class FPSInput : MonoBehaviour{
 
     private void Jump(){
         if(!crouch && onGround){
-            _ySpeed = jumpSpeed;
-            jumpRotation = transform.rotation;
+            _jumping = true;
+            onGround = false;
+            deltaY = Mathf.Sqrt(jumpHeight * -2f * gravity);
+            _jumpRotation = transform.rotation;
         }
     }
     private void Idle(){
         crouch = false;
-        //_camera.transform.localPosition = new Vector3(0f, 0.5f, 0f);
+        _camera.transform.localPosition = new Vector3(0f, 0.5f, 0f);
         speed = FPSInput.WALK_SPEED;     
     }
 
     private void Run(){
         if(onGround){
             speed = FPSInput.RUN_SPEED;  
-            //_camera.transform.localPosition = new Vector3(0f, 0.5f, 0f);
+            _camera.transform.localPosition = new Vector3(0f, 0.5f, 0f);
             crouch = false;
         }
     }
 
     private void MovePlayer(Vector3 movement) {
-        //transform the movement from local to global coordinates
-        if(onGround)
-            movement = transform.TransformDirection(movement);
-        else{
-            Quaternion rot = transform.rotation;
-            transform.rotation = jumpRotation;
-            movement = transform.TransformDirection(movement);
-            transform.rotation = rot;
-        }
+        
+
         _charController.Move(movement);
         // if ((!onGround) && (flags & CollisionFlags.Below) != 0){
         //     actions.RemoveAll(action => action == Actions.Jump);
@@ -107,18 +106,16 @@ public class FPSInput : MonoBehaviour{
             actions.RemoveAll(x => x == Actions.Run);
         }
 
+        if (Input.GetKeyDown(KeyCode.Space)){
+            Jump();
+        }
     } 
 
     private bool detectOnGround(){
-        //  onGround = false;
-        //  if (( _charController.collisionFlags & CollisionFlags.Below) != 0){
-        //     //actions.RemoveAll(action => action == Actions.Jump);
-        //     onGround = true;
-        // }
         bool hitGround = false;
         RaycastHit hit;
-        if(_ySpeed < 0 && Physics.Raycast(transform.position ,Vector3.down, out hit)){
-            float check = (_charController.height+ _charController.radius)/1.9f;
+        if(deltaY < 0 && Physics.Raycast(transform.position ,Vector3.down, out hit)){
+            float check = (_charController.height+ _charController.radius)/0.95f;
             hitGround = hit.distance <= check;
         }
         return hitGround;
@@ -143,30 +140,40 @@ public class FPSInput : MonoBehaviour{
             if (Input.GetButtonDown("Jump")){
                 Jump();
             }else{
-               //Debug.Log("CIAOUS");
-                _ySpeed = minFall;
+                deltaY = _minFall;
                 deltaX = speed;
                 deltaZ = speed;
             }
-            
         }
-        else {
+        else{
             //we omit the friction when deciding to move 
             deltaX = Mathf.Max(0f, deltaX+airResistance*Time.deltaTime);
             deltaZ = Mathf.Max(0f, deltaZ+airResistance*Time.deltaTime);
-            _ySpeed += gravity * 4f *Time.deltaTime;
-            if ( _ySpeed < terminalVelocity){
-                _ySpeed = terminalVelocity;
+            if ( deltaY < _terminalVelocity){
+                deltaY = _terminalVelocity;
             }
-           
         }
+
+
+        deltaY += gravity * Time.deltaTime;
         //applying direction to X and Z delta
         Vector3 velocity = new Vector3(deltaX*dirX, 0, deltaZ*dirZ);
-        velocity = Vector3.ClampMagnitude(velocity, speed);
+        velocity = Vector3.ClampMagnitude(velocity, speed);        
+        //warning, clamp over y, when should we do it?
+        velocity.y = deltaY;
+        if(onGround)
+            velocity = transform.TransformDirection(velocity);
+        else{
+            Quaternion rot = transform.rotation;
+            transform.rotation = _jumpRotation;
+            velocity = transform.TransformDirection(velocity);
+            transform.rotation = rot;
+        }
         if(!onGround){
-             if(_charController.isGrounded){
+            if(_charController.isGrounded){
+                temp =  Vector3.Dot(velocity, _contact.normal);
                 if(Vector3.Dot(velocity, _contact.normal) < 0){
-                    velocity -= _contact.normal * speed;
+                    velocity = _contact.normal * speed;
                 }
                 else{
                     velocity += _contact.normal * speed;
@@ -174,8 +181,6 @@ public class FPSInput : MonoBehaviour{
             }
 
         }
-
-        velocity.y = _ySpeed;
         velocity *= Time.deltaTime;     
         MovePlayer(velocity);
     }
@@ -183,5 +188,4 @@ public class FPSInput : MonoBehaviour{
     void OnControllerColliderHit(ControllerColliderHit hit){
         _contact = hit;
     }
-
 }
